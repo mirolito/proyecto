c27e5f7fe41c655941df02cd9d09983a85c8a5bb
miro <miro@git.com>
2023-03-13 12:50:13 +0000

    vers1

---
+++ JavaDocker.java
@@ -0,0 +1,7 @@
+public class JavaDocker {
+ public static void main(String[] args) {
+ System.out.println("Ejecutando java dentro de docker");
+ System.out.println("¡¡¡Realizado por mí!!!");
+ System.out.println("¡Funciona!");
+ }
+}
